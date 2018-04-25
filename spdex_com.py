f2ed98da5b3ca609a3ccbce96d02dd48963b1026
lxk <619400536@qq.com>
2018-04-25 09:18:02 +0800

    spedx_com 完工

--- spdex_com.py
+++ spdex_com.py
@@ -1,6 +1,7 @@
 import requests
+import  xlwt
 from  bs4 import BeautifulSoup
-
+import os
 def get_token():
     params={
         'client_id': 'ejS0TzdTjGaKUMqNSlA23PTb',
@@ -72,7 +73,7 @@ def login():
     print(r.status_code)
     #print(r.url)
     return s
-token=get_token()
+#token=get_token()
 #ocr=get_ocr(token)
 
 def getallid(s):
@@ -95,7 +96,85 @@ def getallid(s):
 def f(xx):
     return xx['id']
 
+def getxls(id,s,ii):
+    headers={
+        'Host':r'c.spdex.com',
+        'User-Agent': r'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.104 Safari/537.36 Core/1.53.4843.400 QQBrowser/9.7.13021.400',
+    }
+    url=r'http://c.spdex.com/Match/View/Normal/'
+    r=s.get(url+id,headers=headers)
+    soup=BeautifulSoup(r.text,'html.parser')
+    #print(soup.find('title'))
+    try:
+        n=soup.find('td',{'valign': 'bottom','nowrap': 'true',}).text[8:10]
+    except:
+        print('这个没有数据')
+        return
+    n=int(n)
+    print(n,'页','ok')
+    wbk = xlwt.Workbook(encoding='ascii')
+    sheet1 = wbk.add_sheet('sheet1')
+    sheet2 = wbk.add_sheet('sheet2')
+    sheet3 = wbk.add_sheet('sheet3')
+    sheet2.write(0, 0, '时间')
+    sheet2.write(0, 1, '价位')
+    sheet2.write(0, 2, '成家量')
+    sheet2.write(0, 3, '成交变化')
+    sheet2.write(0, 4, '属性')
 
-s=login()
-all_id=getallid(s)
-print('14场比赛id为：',all_id)
+    sheet3.write(0, 0, '时间')
+    sheet3.write(0, 1, '价位')
+    sheet3.write(0, 2, '成家量')
+    sheet3.write(0, 3, '成交变化')
+    sheet3.write(0, 4, '属性')
+
+    sheet1.write(0, 0, '时间')
+    sheet1.write(0, 1, '价位')
+    sheet1.write(0, 2, '成家量')
+    sheet1.write(0, 3, '成交变化')
+    sheet1.write(0, 4, '属性')
+    sheet1.write(0,5,'挂牌倾向')
+    sheet3.write(0,5,'挂牌倾向')
+    sheet2.write(0,5,'挂牌倾向')
+    row=1
+    for i in range(1,n+1):
+        r=s.get(url+id+'/?page='+str(i))
+        soup=BeautifulSoup(r.text,'html.parser')
+        tr=soup.find_all('tr',{'class': False})
+        for x in tr:
+            td=x.find_all('td')
+            if len(td)==16 :
+                time=td[0].text
+                sheet1.write(row,0,time)
+                sheet2.write(row, 0, time)
+                sheet3.write(row, 0, time)
+                sheet1.write(row,1,td[1].text)
+                sheet2.write(row, 1, td[6].text)
+                sheet3.write(row, 1, td[11].text)
+                sheet1.write(row,2,td[2].text)
+                sheet2.write(row,2, td[7].text)
+                sheet3.write(row,2, td[12].text)
+                sheet1.write(row,3,td[3].text)
+                sheet2.write(row, 3, td[8].text)
+                sheet3.write(row,3, td[13].text)
+                sheet1.write(row,4,td[4].text)
+                sheet2.write(row,4, td[9].text)
+                sheet3.write(row,4, td[14].text)
+                sheet1.write(row,5,td[5].text)
+                sheet2.write(row, 5, td[10].text)
+                sheet3.write(row, 5, td[15].text)
+                row=row+1
+    wbk.save(r'spdex_com/'+str(ii)+'.xls')
+
+if __name__=='__main__':
+    s=login()
+    all_id=getallid(s)
+    print('14场比赛id为：',all_id)
+
+    if not os.path.exists('spdex_com'):
+        os.mkdir('spdex_com')
+        print('创建spdex_文件夹')
+    ii = 1  # 后面做文件名
+    for a in range(len(all_id)):
+        getxls(all_id[a],s,ii)
+        ii=ii+1
