a2f951e4f431d0a1b31b4b797e5a6c2cd786218f
Liu <619400536@qq.com>
2019-07-08 16:57:47 +0800

    整理

--- test.py
+++ test.py
@@ -1,52 +1,8 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file '.\test.ui'
-#
-# Created by: PyQt5 UI code generator 5.11.3
-#
-# WARNING! All changes made in this file will be lost!
-
-from PyQt5 import QtCore, QtGui, QtWidgets
-
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(200, 130, 72, 15))
-        self.label.setObjectName("label")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(210, 220, 93, 28))
-        self.pushButton.setObjectName("pushButton")
-        self.toolButton = QtWidgets.QToolButton(self.centralwidget)
-        self.toolButton.setGeometry(QtCore.QRect(360, 220, 47, 21))
-        self.toolButton.setObjectName("toolButton")
-        self.radioButton = QtWidgets.QRadioButton(self.centralwidget)
-        self.radioButton.setGeometry(QtCore.QRect(350, 140, 115, 19))
-        self.radioButton.setObjectName("radioButton")
-        self.checkBox = QtWidgets.QCheckBox(self.centralwidget)
-        self.checkBox.setGeometry(QtCore.QRect(500, 140, 91, 19))
-        self.checkBox.setObjectName("checkBox")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "TextLabel"))
-        self.pushButton.setText(_translate("MainWindow", "PushButton"))
-        self.toolButton.setText(_translate("MainWindow", "..."))
-        self.radioButton.setText(_translate("MainWindow", "RadioButton"))
-        self.checkBox.setText(_translate("MainWindow", "CheckBox"))
-
+while 1:
+    try:
+        input()
+        s=input().split()
+        print(s[-int(input())])
+    except:
+        raise
+        break
